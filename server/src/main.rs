ba2463b0f192fb7794474f9c71ae81857b720fda
glazz <fztmon@gmail.com>
2020-07-06 22:58:42 -0500

    Progress

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,16 +1,14 @@
 // This program uploads selected files to an M5stack
-use std::fs::ReadDir;
-use std::fs::{create_dir, read_dir, DirEntry, File};
+use std::fs::{create_dir, read_dir, read_to_string, DirEntry};
 use std::path::Path;
-use std::io::{Read, stdin};
 fn main() {
-    //Entry scanner
+    //Dir entry scanner
     let path = Path::new("deploy/");
     let dir_data_unsafe = read_dir(path);
     if dir_data_unsafe.is_ok() {
         println!("Deploy directory found!");
     } else {
-        create_dir(path).expect("Could not autocrete 'deploy' directory");
+        create_dir(path).expect("Could not autocreate deploy directory");
     }
     let dir_data = dir_data_unsafe.unwrap();
     let files: Vec<DirEntry> = dir_data
@@ -18,17 +16,37 @@ fn main() {
         .map(|entry| entry.unwrap())
         .collect();
     let mut counter = 0;
-    for file in files { 
+    for file in &files {
         let filename = file.file_name().into_string().unwrap();
-        println!("{}) {}",counter, filename);
+        println!("{}) {}", counter, filename);
         counter += 1;
     }
 
     //user input
     let mut input = String::new();
+    let selection_index: usize;
     loop {
-        
+        println!("Select a file (number)");
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("could not read input");
+        let parsed_unsafe = input.trim().parse::<usize>();
+        if parsed_unsafe.is_ok() {
+            if parsed_unsafe.clone().unwrap() <= counter - 1 {
+                //Correct input
+                selection_index = parsed_unsafe.unwrap();
+                break;
+            } else {
+                println!("Incorrect input, must be less or equal to number of files");
+            }
+        } else {
+            println!("Invalid input");
+        }
+        input.clear();
     }
-    std::io::stdin().read_line(&mut input).expect("could not read input");
-    
+    //File selection fetching
+    let file_string = read_to_string(files[selection_index]
+        .path())
+        .expect("Could not read file");
+    println!("{}",file_string);
 }
